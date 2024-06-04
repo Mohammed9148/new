7e15896d3d0d1ca0e9a249084637a09628b700f9
Mohammed9148 <mohammed.usman.9148@gmail.com>
2024-06-04 18:06:19 +0530

    main.py

--- main.py
+++ main.py
@@ -1,44 +1,17 @@
 import streamlit as st
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from langchain_openai import AzureChatOpenAI
 
-# Load pre-trained model and tokenizer
-@st.cache_resource
-def load_model():
-    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
-    model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
-    return tokenizer, model
+llm = AzureChatOpenAI(
+    model="gpt-35-turbo-16k",
+    deployment_name="VISAGenAI",
+    api_key="dbb69df9354846529d8994cb313275e0",
+    azure_endpoint="https://visagenai.openai.azure.com/",
+    api_version="2024-02-01",
+)
 
-tokenizer, model = load_model()
+st.title("Chat with Azure OpenAI")
+user_input = st.text_input("Type your message here:")
 
-
-# Function to generate chatbot responses using distilGPT-2
-def generate_response(prompt):
-    inputs = tokenizer.encode(prompt, return_tensors="pt")
-    outputs = model.generate(inputs, max_length=150, num_return_sequences=1, no_repeat_ngram_size=2)
-    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return response
-
-# Callback function to handle user input and generate response
-def send_message():
-    user_input = st.session_state.input
-    if user_input:
-        st.session_state.history.append(f"You: {user_input}")
-        response = generate_response(user_input)
-        st.session_state.history.append(f"Bot: {response}")
-        st.session_state.input = ""  # Clear the input after sending
-
-# Streamlit application
-st.title("EleutherAI/gpt-neo-1.3B")
-
-
-# Initialize session state
-if 'history' not in st.session_state:
-    st.session_state.history = []
-
-# User input
-st.text_input("You: ", key="input", on_change=send_message)
-
-# Display conversation history
-for message in st.session_state.history:
-    st.write(message)
-
+if st.button("Send"):
+    response = llm.invoke(user_input)
+    st.write("Response:", response)
